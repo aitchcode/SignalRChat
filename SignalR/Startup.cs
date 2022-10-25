e016a0439ffce86fc136b43e3576dd1f82b7f7e0
hmmdAhmd <hm_mad@hotmail.com>
2022-10-25 11:28:57 +0500

    Add project files.

---
+++ SignalR/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(SignalR.Startup))]
+namespace SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
